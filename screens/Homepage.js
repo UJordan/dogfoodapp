199c4553dd4b0d1ddb4d30fb8a4ea336fb8e744c
JordanU <42947912+UJordan@users.noreply.github.com>
2023-02-26 16:14:58 -0800

    HomePage Setup

--- screens/Homepage.js
+++ screens/Homepage.js
@@ -1,1 +1,13 @@
+import { Text } from "react-native"; 
+
 // Comment
+
+const HomePage = () => {
+    return (
+        <Text>
+            HomePage
+        </Text>
+    );
+};
+
+export default HomePage;
