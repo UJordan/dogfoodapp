83aa4a83ba25a572833b2f31a19886bfe32e3c7f
Hayln Alaine <hayln.alaine@gmail.com>
2023-02-27 21:55:51 -0800

    inventory lists

---
+++ shared/food.js
@@ -0,0 +1,98 @@
+export const FOOD = [
+    {
+        id: 0,
+        brand: 'Canidae',
+        type: 'Pure',
+        flavor: 'Salmon and Sweet Potato',
+        quantity: 10,
+        warehouseLocation: 1
+    },
+    {
+        id: 1,
+        brand: 'Canidae',
+        type: 'Pure',
+        flavor: 'Lamb and Pea',
+        quantity: 30,
+        warehouseLocation: 3
+    },
+    {
+        id: 2,
+        brand: 'Canidae',
+        type: 'Goodness',
+        flavor: 'Real Turkey and Barley',
+        quantity: 20,
+        warehouseLocation: 4
+    },
+    {
+        id: 3,
+        brand: 'Canidae',
+        type: 'Sustain',
+        flavor: 'Planet-Friendly Cage-Free Chicken',
+        quantity: 10,
+        warehouseLocation: 8
+    },
+    {
+        id: 4,
+        brand: 'Nulo',
+        type: 'Medalseries',
+        flavor: 'Baked and Brothy Chicken, Duck, Turkey',
+        quantity: 20,
+        warehouseLocation: 10
+    },
+    {
+        id: 5,
+        brand: 'Nulo',
+        type: 'Medalseries',
+        flavor: 'High-Protein Chicken and Peas',
+        quantity: 30,
+        warehouseLocation: 11
+    },
+    {
+        id: 10,
+        brand: 'Nulo',
+        type: 'Medalseries',
+        flavor: 'High-Protein Turkey, Whitefish, Quinoa',
+        quantity: 10,
+        warehouseLocation: 12
+    },
+    {
+        id: 6,
+        brand: 'Earthborn',
+        type: 'Grain Free',
+        flavor: 'Great Plains Feast',
+        quantity: 60,
+        warehouseLocation: 14
+    },
+    {
+        id: 7,
+        brand: 'Earthborn',
+        type: 'Grain Free',
+        flavor: 'Meadow Fest',
+        quantity: 20,
+        warehouseLocation: 16
+    },
+    {
+        id: 11,
+        brand: 'Earthborn',
+        type: 'Grain Free',
+        flavor: 'Primitive Natural',
+        quantity: 30,
+        warehouseLocation: 17
+    },
+    {
+        id: 8,
+        brand: 'Earthborn',
+        type: 'Venture',
+        flavor: 'Smoked Bison and Pumpkin',
+        quantity: 10,
+        warehouseLocation: 18
+    },
+    {
+        id: 9,
+        brand: 'Earthborn',
+        type: 'Venture',
+        flavor: 'Duck Meal and Pumpkin',
+        quantity: 10,
+        warehouseLocation: 18
+    }
+];
